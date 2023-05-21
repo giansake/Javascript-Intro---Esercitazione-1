25c52df56e7979e7a36abe11c9ca4722a0714181
simcesta <sim.cestari@gmail.com>
2023-05-21 23:06:55 +0200

    Cambio colore testo totalmente inutile :)))

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    const element = document.querySelector('.container');
+    element.style.color = 'white';
+});
+
