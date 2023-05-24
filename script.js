efa1dd66dcfc4c8042e4f83c2b7d962122017ecf
Bea <beatrice.fan75@gmail.com>
2023-05-24 12:03:08 +0200

    updated function

--- script.js
+++ script.js
@@ -2,16 +2,12 @@ let changeColorCard = document.getElementsByClassName("card");
 let hereIsTheMagic = document.getElementById("btn");
 
 function magic() {
-  if (changeColorCard.className === "card") {
-    hereIsTheMagic.className = "card-orange";
+  if ((changeColorCard.className = "card-orange")) {
+    changeColorCard.className = "card-orange";
   } else {
-    hereIsTheMagic.className = "card-blue";
+    changeColorCard.className = "card-blue";
   }
 }
 
 // se la classe di card è card (lo è sempre) allora il colore del bottone sarà arancione
 // se no sarà blu
-
-// la condizione if dovrebbe essere "se il bottone non è ancora premuto" e non
-// se la carta ha la classe card
-//prima il bottone cambiava colore in arancione ora in blu...
