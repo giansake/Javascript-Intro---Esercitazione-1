b8091e110f5aa4772705266cc0d9e562264ac34e
Giammarco <giammarcogaudenzi@gmail.com>
2023-05-27 11:09:51 +0200

    optimization

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-function handleClick (HTMLElement) {
-    if (HTMLElement.innerText === "Here is the magic!") {
-        HTMLElement.innerText = "Back to normal"; 
-        HTMLElement.classList.remove ("active");
-    } else {
-        HTMLElement.innerText = "Here is the magic!";
-        HTMLElement.classList.add("active");
-    }
+function toggleElementsClass() {
+  document.getElementById("switch-card").classList.toggle("magic-card");
+  document.getElementById("switch-card").classList.toggle("card");
 }
 
-function handleClick (HTMLElement) {
-    if ( document.getElementById("switch-card").classList.contains('card') )
-    document.getElementById("switch-card").classList.toggle('magic-card');
-    }
+function updateButton(element, active, text) {
+  element.innerText = text;
+  if (active) {
+    element.classList.remove("active");
+  } else {
+    element.classList.add("active");
+  }
+}
+
+function handleClick(button) {
+  if (document.getElementById("switch-card").classList.contains("card")) {
+    toggleElementsClass();
+    updateButton(button, true, "Back to normal");
+  } else {
+    toggleElementsClass();
+    updateButton(button, false, "Here is the magic!");
+  }
+}
